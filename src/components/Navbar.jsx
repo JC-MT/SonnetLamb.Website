9f469a5954b7926bcb763848d94a778500da7e4f
Jan Matias <jancarlosmatias@gmail.com>
2023-07-26 13:33:34 -0400

    Adds Navbar component and boilerplate router logic

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import './Navbar.scss'
+
+export default function NavBar() {
+
+  return (
+    <div className='navbarContainer'>
+
+        <div className='navbarContainer__name'>
+            <Link to={'/'}>Sonnet Lamb</Link>
+        </div>
+        <div className='navbarContainer__links'>
+            <Link to={'/'}><p>Home</p></Link>
+            <Link to={'/'}><p>About</p></Link>
+            <Link to={'/'}><p>Gallery</p></Link>
+            <Link to={'/'}><p>Reels</p></Link>
+            <Link to={'/'}><p>News</p></Link>
+            <Link to={'/'}><p>Contact</p></Link>
+        </div>
+    </div>
+  )
+}
